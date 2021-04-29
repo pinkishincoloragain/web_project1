aedbbd15bba29cde83fadaced6215f78992003ed
pinkishincoloragain <smb1103@gmail.com>
2021-04-29 14:58:14 +0900

    added royal flush, high card

--- handleCard.js
+++ handleCard.js
@@ -1,4 +1,4 @@
-// 총 카트 갯수는 4*13 = 52
+// 총 카드 갯수는 4*13 = 52
 
 var player1 = new Array();
 var player2 = new Array();
@@ -99,7 +99,7 @@ function revealCard() {
 
     setTimeout(function() {
         flipCard();
-    }, 3000);
+    }, 1000);
 
 }
 
@@ -116,11 +116,13 @@ function setCard() {
 }
 
 function roll() {
+    alert("판을 엎습니다.");
     for (var i = 0; i <= 2160; i++) {
         setTimeout("rotateit(" + i + ")", i);
     }
 
     newGame();
+
 }
 
 function rotateit(x) {
@@ -146,38 +148,78 @@ function newGame() {
     for (i = 0; i < 52; i++) {
         allCards[i] = i;
     }
+
 }
 
 
 function calRank() {
-    calculate(player1);
+    var res;
+    res = calculate(player1);
+    alert(res);
+
     calculate(player2);
     calculate(player3);
     calculate(player4);
-}
 
+}
 
 function calculate(arr) {
 
+
     len = arr.length;
+
     var num = new Array(len);
     var alpha = new Array(len);
+    var notsorted_alpha = new Array(len);
+    var notsorted_num = new Array(len);
+
+    var temp = new Array(len);
     var nu;
     var al;
 
+
+
     // S > D > H > C
+    symbols = ["S", "D", "H", "C"];
+    real_symbols = ["Spade", "Diamond", "Heart", "Clover"];
+
+    // sort cards
     for (var i = 0; i < len; i++) {
-        temp = arr[i];
-        if (Number.isInteger(parseInt(temp[1]))) {
-            nu = parseInt(temp[0] + temp[1]);
-            al = temp[2];
+        cur = arr[i];
+        if (Number.isInteger(parseInt(cur[1]))) {
+            nu = parseInt(cur[0] + cur[1]);
+            al = cur[2];
 
         } else {
-            nu = temp[0];
-            al = temp[1];
+            nu = cur[0];
+            al = cur[1];
         }
+
         num[i] = nu;
+        notsorted_num[i] = nu;
         alpha[i] = al;
+        notsorted_alpha[i] = al;
+
+    }
+
+
+
+    num.sort(function(a, b) { return a - b });
+
+    for (i = 0; i < len; i++) {
+        for (var j = 0; j < len; j++) {
+            if (num[i] == notsorted_num[j]) {
+                alpha[i] = notsorted_alpha[j];
+                notsorted_num.splice(j, 1);
+                notsorted_alpha.splice(j, 1);
+                break;
+            }
+        }
+    }
+
+    for (i = 0; i < len; i++) {
+        alert(num[i]);
+        alert(alpha[i]);
     }
 
     // for (i = 0; i < len; i++) {
@@ -188,11 +230,44 @@ function calculate(arr) {
     // }
 
     // Royal Flush
+    var cnt = 0;
 
-    var rf = "Royal Flush"
+    function RoyalFlush() {
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < len; j++) {
+                if (symbols[i] == alpha[j]) {
+                    temp[cnt] = i;
+                    cnt++;
+                }
+            }
+            if (cnt > 5) {
+                if (alpha[0] == 1 &&
+                    alpha[1] == 10 &&
+                    alpha[2] == 11 &&
+                    alpha[3] == 12 &&
+                    alpha[4] == 13) {
+                    return (alpha[0] + " Royal flush");
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    var res = RoyalFlush();
+    if (res != -1)
+        return res;
 
 
     // Straight Flush
+    function StraightFlush() {
+        sf1 = num[0];
+        for (i = 0; i < len; i++) {
+
+        }
+    }
+
+
 
     // Four of a Kind
 
@@ -205,6 +280,82 @@ function calculate(arr) {
     // Three of a Kind
 
     // Two Pair
+    function TwoPair() {
+        var pairIdx1 = -1;
+        var pairIdx2 = -1;
+        for (i = 0; i < len - 1; i++) {
+            if (num[i] == num[i + 1]) {
+                pairIdx1 = i;
+            }
+            break;
+        }
+        for (i = pairIdx1; i < len - 1; i++) {
+            if (num[i] == num[i + 1]) {
+                pairIdx2 = i;
+            }
+            break;
+        }
+        alert(pairIdx1);
+        alert(pairIdx);
+
+        if (pairIdx != -1) {
+            var real = 0;
+            for (real = 0; real < 4; real++) {
+                if (symbols[real] == alpha[pairIdx]) {
+                    return (real_symbols[real] + " " + num[pairIdx] + " " + real_symbols[real] + " " + num[pairIdx + 1] + " Two Pair");
+                }
+            }
+        }
+        return -1;
+    }
+
+    res = TwoPair();
+    if (res != -1)
+        return res;
+
+    // One Pair
+    function OnePair() {
+        var pairIdx = -1;
+        for (i = 0; i < len - 1; i++) {
+            if (num[i] == num[i + 1]) {
+                pairIdx = i;
+            }
+        }
+        alert(pairIdx);
+
+        if (pairIdx != -1) {
+            var real = 0;
+            for (real = 0; real < 4; real++) {
+                if (symbols[real] == alpha[pairIdx]) {
+                    return (real_symbols[real] + " " + num[pairIdx] + " " + real_symbols[real] + " " + num[pairIdx + 1] + " Two Pair");
+                }
+            }
+        }
+        return -1;
+    }
+
+    res = OnePair();
+    if (res != -1)
+        return res;
 
     // High Card
+    var maxNum = 0;
+    var maxIdx = 0;
+
+    for (i = 0; i < len; i++) {
+        if (maxNum < num[i]) {
+            maxNum = num[i];
+            maxIdx = i;
+        }
+    }
+
+
+    var real = 0;
+    for (real = 0; real < 4; real++) {
+        if (symbols[real] == alpha[maxIdx])
+            break;
+    }
+
+    return real_symbols[real] + " " + num[maxIdx] + " High Card";
+
 }
